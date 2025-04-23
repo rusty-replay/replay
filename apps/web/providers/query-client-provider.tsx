95e7ec19e39ffcbb01e89eaeb7701d4089e6ddf5
Jaeha Lee <wogkdkrm112@gmail.com>
2025-04-23 21:25:02 +0900

    feat: remove redundant code

--- apps/web/providers/query-client-provider.tsx
+++ apps/web/providers/query-client-provider.tsx
@@ -5,14 +5,9 @@ import {
   QueryClient,
   QueryClientProvider as ReactQueryClientProvider,
 } from '@tanstack/react-query';
-import { startRecording } from '@/utils/record';
 
 export default function QueryClientProvider({ children }: PropsWithChildren) {
   const queryClient = new QueryClient({});
-
-  useEffect(() => {
-    startRecording();
-  }, []);
 
   return (
     <ReactQueryClientProvider client={queryClient}>
