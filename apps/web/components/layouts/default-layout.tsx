b451d46fed183f907feaf3f22ec9074e011cf43f
Jaeha Lee <wogkdkrm112@gmail.com>
2025-04-26 14:06:54 +0900

    feat: theme mode switcher nav 오른쪽 배치

--- apps/web/components/layouts/default-layout.tsx
+++ apps/web/components/layouts/default-layout.tsx
@@ -23,7 +23,7 @@ export default function DefaultLayout({ children }: { children: ReactNode }) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 justify-between">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
@@ -39,7 +39,7 @@ export default function DefaultLayout({ children }: { children: ReactNode }) {
               </BreadcrumbList>
             </Breadcrumb>
           </div>
-          <div>
+          <div className="px-4">
             <ModeSwitcher />
           </div>
         </header>
