36214f8572a93efb42b38ac408f1fa0523971101
Jaeha Lee <wogkdkrm112@gmail.com>
2025-05-06 04:50:23 +0900

    fix: useGetEventParams - issue_id rollback

--- apps/web/components/project/hooks/use-get-event-params.ts
+++ apps/web/components/project/hooks/use-get-event-params.ts
@@ -1,9 +1,8 @@
 import { useParams } from 'next/navigation';
-import React from 'react';
 
 export function useGetEventParams() {
-  const { event_id: eventId } = useParams<{
-    event_id: string;
+  const { issue_id: eventId } = useParams<{
+    issue_id: string;
   }>();
 
   return { eventId: eventId ? parseInt(eventId) : undefined };
